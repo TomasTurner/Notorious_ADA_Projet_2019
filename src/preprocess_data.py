cedda17da9da045f0bf8846d08161700fa736781
Tristan Trébaol <tristan.trebaol@epfl.ch>
2019-12-20 04:22:57 +0100

    code runs, religion dataset sanitized, code shorter and structured

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -6,24 +6,18 @@ import csv
 projectdata_PATH = '../data/projectdata/'
 dataprocessed_PATH = '../data/dataprocessed/'
 
-def preprocess_data_file(Filename, newFilename): # for Tax and Liscence
+def preprocess_data_file(Filename, newFilename): # for Tax and Liscence public use
     tempFilename = dataprocessed_PATH + 'tempFile.csv'
-    tempFilename1 = dataprocessed_PATH + 'tempFile1.csv'
     text_to_bool(Filename, tempFilename)
-    merge_str_as_header(tempFilename, tempFilename1)
-    leave_out_nodata(tempFilename1, newFilename)
+    merge_str_as_header(tempFilename, newFilename)
     
 def preprocess_data_file_one(Filename, newFilename): # for health_warning
-    tempFilename = dataprocessed_PATH + 'tempFile.csv'
-    text_to_bool(Filename, tempFilename)
-    leave_out_nodata_one(tempFilename, newFilename)
+    text_to_bool(Filename, newFilename)
 
-def preprocess_public_use_data(Filename, newFilename): # for community_actions
+def preprocess_public_use_data(Filename, newFilename):
     tempFilename = dataprocessed_PATH + 'tempFile.csv'
-    tempFilename1 = dataprocessed_PATH + 'tempFile1.csv'
     text_to_number(Filename, tempFilename)
-    merge_str_as_header(tempFilename, tempFilename1)
-    leave_out_nodata_multi(tempFilename1, newFilename)
+    merge_str_as_header(tempFilename, newFilename)
     
 def preprocess_age_limits(Filename, newFilename): #for age limits
     tempFilename = dataprocessed_PATH + 'tempFile.csv'
@@ -31,8 +25,8 @@ def preprocess_age_limits(Filename, newFilename): #for age limits
     df=pd.read_csv(tempFilename, sep=',')
     #sanitize
     HEURISTIC_TOTAL_BAN = 50
-    df=df.replace({'total ban': 'Total ban',
-                'subnational': 'Subnational',
+    df=df.replace({'total ban': HEURISTIC_TOTAL_BAN,
+                'subnational': np.nan,
                 'Total ban': HEURISTIC_TOTAL_BAN,
                 'None':0,
                 'Subnational': np.nan,
@@ -45,46 +39,30 @@ def preprocess_age_limits(Filename, newFilename): #for age limits
     df.to_csv(newFilename, index=False, sep=',')
     
     
-# function for process text data, capitalize, clear out white space and '-', especially for country name
+# function to process text data, capitalize, clear out white space and '-', especially for country name
 def process_text(origin_text):
     new_text = origin_text.capitalize()
     new_text = ''.join(new_text.split('-'))
     new_text = ''.join(new_text.split(' '))
     return new_text
 
-# function for process country name, call def process_text
+# function to process country name, call def process_text
 def process_country_name(Filename, newFile_PATH, column):
     df = pd.read_csv(Filename, header=None, sep=',')
     for row in df.iterrows():
         country_name = row[1][column]
         if isinstance(country_name,str):
             row[1][column]=process_text(country_name)
+    df=df.dropna(axis='rows', how='any')
     df.to_csv(newFile_PATH, header=None, index=False, sep=',')
     df.head()
-
-# funciton for deal with 'No data', considering about different number of columns
-# however can be replaced by dropna function
-def leave_out_nodata(Filename, newFile_PATH):
-    df = pd.read_csv(Filename, header=None, sep=',')
-    df = df[(df[2]!='No data')&(df[3]!='No data')&(df[4]!='No data')]
-    df.to_csv(newFile_PATH,header=None,index=False,sep=',')
-
-def leave_out_nodata_one(Filename, newFile_PATH):
-    df = pd.read_csv(Filename, header=None, sep=',')
-    df = df[df[2]!='No data']
-    df.to_csv(newFile_PATH,header=None,index=False,sep=',')
-    
-def leave_out_nodata_multi(Filename, newFile_PATH):
-    df = pd.read_csv(Filename, header=None, sep=',')
-    df = df[(df[2]!='No data')&(df[3]!='No data')&(df[4]!='No data')&(df[5]!='No data')&(df[6]!='No data')&(df[7]!='No data')&(df[8]!='No data')&(df[9]!='No data')&(df[10]!='No data')]
-    df.to_csv(newFile_PATH,header=None,index=False,sep=',')
     
 # dict for file countriy_list_final, input country name and get iso3
 def dict_country_code3(inputcountry):
     with open(projectdata_PATH + 'Country_list.csv','r',encoding="utf-8") as f:
         reader = csv.reader(f)
         fieldnames = next(reader) #get first row，as key for the dictionary get from generator next
-        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 以list的形式存放键名
+        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict
         for row in csv_reader:
             d = {}
             for k,v in row.items():
@@ -99,7 +77,7 @@ def dict_country_region(inputcountry):
     with open(projectdata_PATH + 'Country_list.csv','r',encoding="utf-8") as f:
         reader = csv.reader(f)
         fieldnames = next(reader) #get first row，as key for the dictionary get from generator next
-        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 以list的形式存放键名
+        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 
         for row in csv_reader:
             d = {}
             for k,v in row.items():
@@ -114,7 +92,7 @@ def dict_country_religion(inputcountry):
     with open(projectdata_PATH + 'Country_list.csv','r',encoding="utf-8") as f:
         reader = csv.reader(f)
         fieldnames = next(reader) #get first row，as key for the dictionary get from generator next
-        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 以list的形式存放键名
+        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 
         for row in csv_reader:
             d = {}
             for k,v in row.items():
@@ -128,7 +106,7 @@ def dict_country_code2(inputcode3):
     with open(projectdata_PATH +'iso_countries_list.csv','r',encoding="utf-8") as f:
         reader = csv.reader(f)
         fieldnames = next(reader)
-        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 以list的形式存放键名
+        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 
         for row in csv_reader:
             d = {}
             for k,v in row.items():
@@ -143,7 +121,7 @@ def dict_country_latitude(inputcode2):
     with open(projectdata_PATH + 'Country_location.csv','r',encoding="utf-8") as f:
         reader = csv.reader(f)
         fieldnames = next(reader)
-        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 以list的形式存放键名
+        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 
         for row in csv_reader:
             d = {}
             for k,v in row.items():
@@ -158,7 +136,7 @@ def dict_country_longitude(inputcode2):
     with open(projectdata_PATH + 'Country_location.csv','r',encoding="utf-8") as f:
         reader = csv.reader(f)
         fieldnames = next(reader)
-        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 以list的形式存放键名
+        csv_reader = csv.DictReader(f,fieldnames=fieldnames) #self._fieldnames = fieldnames   # list of keys for the dict 
         for row in csv_reader:
             d = {}
             for k,v in row.items():
@@ -215,26 +193,26 @@ def process_countries(Filename, newFile_PATH):
     df = df[['Number', 'Country', 'iso2', 'iso3', 'latitude', 'longitude', 'Region', 'Religion'] ]
     df.to_csv(newFile_PATH, index=False, header=None, sep=',')
     
-    
-    
-    
-    # preprocess policy data files
+
+# replaces yes and no to values, while setting 'No data' to NaN
 def text_to_bool(Filename, new_file_PATH):
     df = pd.read_csv(Filename, header=None, sep=',')
-    df.replace('Yes', '1', inplace=True)
-    df.replace('No', '0', inplace=True)
-    df.replace('Total ban', '1.5', inplace=True)
+    df.replace({'Yes': 1,
+                'No': 0,
+                'Total ban': 1.5,
+                'No data': np.nan}, 
+               inplace=True)
     df.to_csv(new_file_PATH, header=None, index=False, sep=',')
+
 
 def text_to_number(Filename, new_file_PATH):
     df = pd.read_csv(Filename, header=None, sep=',')  
-    df.replace('ban', '1', inplace=True)
-    df.replace('partial restriction', '0.5', inplace=True)
-    df.replace('voluntary/self-restricted', '0.3', inplace=True)
-    df.replace('no restrictions', '0', inplace=True)
+    df.replace('ban', 1, inplace=True)
+    df.replace('partial restriction', 0.5, inplace=True)
+    df.replace('voluntary/self-restricted', 0.3, inplace=True)
+    df.replace('no restrictions', 0, inplace=True)
+    df.replace('No data', np.nan, inplace=True)
     df.to_csv(new_file_PATH, header=None, index=False, sep=',')
-    
-    
    
 '''
 target: merge the string of the two first rows of the dataframe
@@ -269,7 +247,7 @@ def get_unique_values(df):
     return list(val1.unique())
     
 
-# scales the dataframe to the wanted range
+# scales the dataframe to the specified range (0,1) by default
 def rescale_data(df, feature_range=(0,1)):
     mm_scaler = MinMaxScaler(feature_range=feature_range)
     df_mm = mm_scaler.fit_transform(df)
